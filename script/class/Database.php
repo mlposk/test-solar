e880271fee18b9d9961f4773acd5e8d325709617
Alexander Molokotin <mlposk@yandex.ru>
2020-02-08 21:21:33 +0500

    set charset

--- script/class/Database.php
+++ script/class/Database.php
@@ -33,6 +33,8 @@ class Database
             $this->dbName
         );
 
+        $this->connection->set_charset("utf8");
+
         if($this->connection->connect_error) {
             echo "Fail. Info: " . $this->connection->connect_error;
         }
