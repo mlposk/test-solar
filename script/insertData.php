74595497bf618bce3c0b2ec487a60d4314b9b0e1
Aleksander Molokotin <admin@mlposk.ml>
2020-02-10 00:44:33 +0500

    add query of getting rows count

--- script/insertData.php
+++ script/insertData.php
@@ -5,4 +5,5 @@ if (isset($_POST['tasks'])) {
     foreach ($_POST['tasks'] as $index => $name) {
         Query::insertTask(array_values($name));
     }
+    echo json_encode(array("total" => Query::getCount()[0]), JSON_UNESCAPED_UNICODE);
 }
