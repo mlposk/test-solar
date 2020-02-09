46e1c73c97669fc58474128d33e86bc725ecfd33
Aleksander Molokotin <admin@mlposk.ml>
2020-02-10 00:43:30 +0500

    add queries of getting rows data and rows count

--- script/getData.php
+++ script/getData.php
@@ -2,7 +2,7 @@
 include_once("class/Query.php");
 
 if (isset($_POST['limit']) && isset($_POST['offset'])) {
-    echo json_encode(array("rows" => Query::getRows($_POST['limit'], $_POST['offset'])), JSON_UNESCAPED_UNICODE);
+    echo json_encode(array("rows" => Query::getRows($_POST['limit'], $_POST['offset']), "total" => Query::getCount()[0]), JSON_UNESCAPED_UNICODE);
 } else {
     echo json_encode(array("total" => Query::getCount()[0]), JSON_UNESCAPED_UNICODE);
 }
